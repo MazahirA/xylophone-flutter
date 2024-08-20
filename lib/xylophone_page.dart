f235dac3d001daece518ee585cbd421b3d24fbc9
mali <mazahir.ali2k17@gmail.com>
2024-08-21 03:51:32 +0500

    added safe area, and reduced code redundancy by using functions.

--- lib/xylophone_page.dart
+++ lib/xylophone_page.dart
@@ -2,86 +2,45 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class XylophoneApp extends StatelessWidget {
-  XylophoneApp({super.key});
-  final player = AudioPlayer();
+  const XylophoneApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: InkWell(
-              child: const Card(
-                color: Colors.red,
-              ),
-              onTap: () async {
-                await player.play(AssetSource('note1.wav'));
-              },
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              child: const Card(
-                color: Colors.orange,
-              ),
-              onTap: () async {
-                await player.play(AssetSource('note2.wav'));
-              },
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              child: const Card(
-                color: Colors.yellow,
-              ),
-              onTap: () async {
-                await player.play(AssetSource('note3.wav'));
-              },
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              child: const Card(
-                color: Colors.green,
-              ),
-              onTap: () async {
-                await player.play(AssetSource('note4.wav'));
-              },
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              child: const Card(
-                color: Colors.blue,
-              ),
-              onTap: () async {
-                await player.play(AssetSource('note5.wav'));
-              },
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              child: const Card(
-                color: Colors.indigo,
-              ),
-              onTap: () async {
-                await player.play(AssetSource('note6.wav'));
-              },
-            ),
-          ),
-          Expanded(
-            child: InkWell(
-              child: const Card(
-                color: Colors.deepPurple,
-              ),
-              onTap: () async {
-                await player.play(AssetSource('note7.wav'));
-              },
-            ),
-          ),
-        ],
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            buildXylophoneKey(color: Colors.red,soundNoteNum: 1),
+            buildXylophoneKey(color: Colors.orange,soundNoteNum: 2),
+            buildXylophoneKey(color: Colors.yellow,soundNoteNum: 3),
+            buildXylophoneKey(color: Colors.green,soundNoteNum: 4),
+            buildXylophoneKey(color: Colors.blue,soundNoteNum: 5),
+            buildXylophoneKey(color: Colors.indigo,soundNoteNum: 6),
+            buildXylophoneKey(color: Colors.purple,soundNoteNum: 7),
+          ],
+        ),
+      ),
+    );
+  }
+
+  ///Play sound according to note number.
+  void playSound({required int noteNum}){
+    final player = AudioPlayer();
+    player.play(AssetSource('note$noteNum.wav'));
+  }
+  
+  ///Build note keys:
+  Expanded buildXylophoneKey({required Color color,required int soundNoteNum}){
+    return Expanded(
+      child: InkWell(
+        child:  Card(
+          color: color,
+        ),
+        onTap: ()  {
+          playSound(noteNum: soundNoteNum);
+        },
       ),
     );
   }
